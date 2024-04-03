8ebc1c50182abcbfdc4590280d5aaf4828cfb441
Sunayana1111 <sthasunayana29@gmail.com>
2024-04-03 19:34:33 +0545

    add media url

--- account/views/login.py
+++ account/views/login.py
@@ -12,4 +12,5 @@ class LoginView(ObtainAuthToken):
             'message': "Login Successful !!",
             'token': token.key,
             'username': user.username
-            }) 
+            })
+ 
--- happy_home/urls.py
+++ happy_home/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.conf import settings
 from django.urls import path, include
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -18,3 +19,5 @@ if settings.DEBUG:
         path('api-auth/', include('rest_framework.urls',
                               namespace='rest_framework')),
     ]
+
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
